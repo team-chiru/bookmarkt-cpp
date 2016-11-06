ffea5af323673ee22753fe7f9230b6339d401f1e
vlnk <vlnk@mail.com>
2016-11-06 01:39:29 -0500

    finally success to build with cmake

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,21 @@
 from conans import ConanFile, CMake
 
 class BookmarktConan(ConanFile):
-   settings = "os", "compiler", "build_type", "arch"
-   # requires = "Poco/1.7.3@lasote/stable" # comma separated list of requirements
+    settings = "os", "compiler", "build_type", "arch"
+    generators  = "cmake", "txt"
+    # default_options = "Poco:shared=True", "OpenSSL:shared=True"
 
-   generators = "cmake", "gcc", "txt"
-   # default_options = "Poco:shared=True", "OpenSSL:shared=True"
+    def configure(self):
+        if self.settings.os == "Linux":
+            self.settings.compiler = "clang"
+            self.settings.compiler.libcxx = "libstdc++11"
+            self.settings.compiler.version = "3.8"
 
-   def imports(self):
-      self.copy("*.dll", dst="bin", src="bin") # From bin to bin
-      self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+    def imports(self):
+        self.copy("*.dll", dst = "bin", src = "bin") # From bin to bin
+        self.copy("*.dylib*", dst = "bin", src = "lib") # From lib to bin
 
-   def build(self):
-      cmake = CMake(self.settings)
-      self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
-      self.run('cmake --build . %s' % cmake.build_config)
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+        self.run('cmake --build . %s' % cmake.build_config)
