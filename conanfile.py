eaf1b63b9751e038e3cc7ed3764e19a9f2c8628d
vlnk <vlnk@mail.com>
2016-11-05 22:47:45 -0400

    init conan

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conans import ConanFile, CMake
+
+class PocoTimerConan(ConanFile):
+   settings = "os", "compiler", "build_type", "arch"
+   # requires = "Poco/1.7.3@lasote/stable" # comma separated list of requirements
+
+   generators = "cmake", "gcc", "txt"
+   # default_options = "Poco:shared=True", "OpenSSL:shared=True"
+
+   def imports(self):
+      self.copy("*.dll", dst="bin", src="bin") # From bin to bin
+      self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+
+   def build(self):
+      cmake = CMake(self.settings)
+      self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+      self.run('cmake --build . %s' % cmake.build_config)
